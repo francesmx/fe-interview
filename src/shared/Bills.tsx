81c47ce2bf10ec7e5bd45628e2a1e534ea4490d6
Frances Maxwell <frances.maxwell@skyscanner.net>
2021-12-22 00:23:29 +0000

    Implements patch request for updating merchants

--- src/shared/Bills.tsx
+++ src/shared/Bills.tsx
@@ -21,8 +21,10 @@ type Merchant = {
 };
 
 export const Bills: React.FC = () => {
-  const URL = `${APIConstants.base}/merchants`;
-  const { data, loading } = useFetch(URL, []);
+  const MERCHANTS_URL = `${APIConstants.base}/merchants`;
+  const { data, loading } = useFetch(MERCHANTS_URL, []);
+  const [request, response] = useFetch(MERCHANTS_URL);
+
   const [viewBills, setViewBills] = useState(true);
 
   const handleClickBills = () => {
@@ -31,6 +33,27 @@ export const Bills: React.FC = () => {
 
   const handleClickPotentialBills = () => {
     setViewBills(false);
+  };
+
+  // The patch request is working.
+  // However the following request.get isn't getting the new data
+  // We also have data in two objects (data and response.data)
+  // But we don't want the data object to be mutable
+  // Can re-look at use-http docs to see how they manage handling onMount and onClick events together
+  // Should probably do that AND considering using Redux at this point to manage state
+
+  const handleClickRemoveBill = async (merchantId: string) => {
+    await request.patch(`/${merchantId}`, { isBill: false });
+    if (response.ok) {
+      await request.get();
+    }
+  };
+
+  const handleClickAddAsBill = async (merchantId: string) => {
+    await request.patch(`/${merchantId}`, { isBill: true });
+    if (response.ok) {
+      await request.get();
+    }
   };
 
   return (
@@ -45,13 +68,17 @@ export const Bills: React.FC = () => {
             <div key={merchant.id} style={{ marginTop: 30, width: 500, textAlign: 'left' }}>
               <Img
                 src={[merchant.iconUrl, cleoCoin]}
-                loader={<Img src={loaderGif} />}
+                loader={<Img src={loaderGif} style={{ width: 50, height: 50, float: 'left' }} />}
                 alt={`${merchant.name} logo`}
                 style={{ width: 50, height: 50, float: 'left' }}
               />
               <h3>{merchant.name}</h3>
               <p>{merchant.transactions.length} transactions</p>
-              {merchant.isBill ? <button>Remove bill</button> : <button>Add as bill</button>}
+              {merchant.isBill ? (
+                <button onClick={() => handleClickRemoveBill(merchant.id)}>Remove bill</button>
+              ) : (
+                <button onClick={() => handleClickAddAsBill(merchant.id)}>Add as bill</button>
+              )}
             </div>
           );
         })}
