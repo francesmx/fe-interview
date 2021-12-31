c59463f5f4bb9614cf240e35c90a6f3ac0cd4d31
Frances Maxwell <frances.maxwell@skyscanner.net>
2021-12-31 02:44:05 +0000

    Improves TransactionsRow

--- src/components/transactions/TransactionsRow.tsx
+++ src/components/transactions/TransactionsRow.tsx
@@ -5,20 +5,19 @@ interface TransactionRowProps {
   transaction: TransactionType;
 }
 
-/* Date format is 1 Aug 2018 */
-const formattedTransactionDate = (date: string) => {
-  return format(Date.parse(date), 'd MMM y');
-};
-
-/* GBP currency formatting */
-const formattedTransactionAmount = (amount: number) => {
-  return new Intl.NumberFormat('en-GB', {
-    style: 'currency',
-    currency: 'GBP',
-  }).format(amount);
-};
-
 export const TransactionRow: React.FC<TransactionRowProps> = ({ transaction }) => {
+  /* Date format is 1 Aug 2018 */
+  const formattedTransactionDate = (date: string) => {
+    return format(Date.parse(date), 'd MMM y');
+  };
+
+  const formattedTransactionAmount = (amount: number) => {
+    return new Intl.NumberFormat('en-GB', {
+      style: 'currency',
+      currency: 'GBP',
+    }).format(amount);
+  };
+
   return (
     <tr key={transaction.id}>
       <td style={{ textAlign: 'left' }}>{formattedTransactionDate(transaction.date)}</td>
